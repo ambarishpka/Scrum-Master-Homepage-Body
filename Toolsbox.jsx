e112dce7942daf94a2ecd40895e7ba860bc2877b
AmbarishPK <41122980+Ambarishpk@users.noreply.github.com>
2019-05-23 15:35:58 +0530

    Add files via upload
    tools box bug cleared

--- Toolsbox.jsx
+++ Toolsbox.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './toolsbox.css'
-import toolspng from '../images/tools.png'
+import toolspng from '../images/tools/tools.png'
+import toolsjira from '../images/bitmap/bitmap.png'
 
 export default class Toolsbox extends Component {
     render() {
@@ -8,13 +9,247 @@ export default class Toolsbox extends Component {
             <div className="tools-box-main">
                 <div className="tools-box shadow-lg">
                     <div className="oval-tools d-flex justify-content-center align-items-center">
-                        <div className="font-weight-bold text-white"><img src={toolspng} className="oval-tools-image" alt="Dot" /></div>
+                        <div><img src={toolspng} className="oval-tools-image" alt="Dot" /></div>
                     </div>
                     <div className="tools-box-header">
                         Tools
                     </div>
                     <div className="tools-box-body">
-                        <div className="project-tool">Project Tool</div>
+                        <div className="project-tool">
+                            Project Tool
+                        </div>
+                        <div className="rectangle">
+                            <div className="rectangle-custom">
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="project-tool">
+                            Project Tool
+                        </div>
+                        <div className="rectangle">
+                            <div className="rectangle-custom">
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="project-tool">
+                            Project Tool
+                        </div>
+                        <div className="rectangle">
+                            <div className="rectangle-custom">
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="project-tool">
+                            Project Tool
+                        </div>
+                        <div className="rectangle">
+                            <div className="rectangle-custom">
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="project-tool">
+                            Project Tool
+                        </div>
+                        <div className="rectangle">
+                            <div className="rectangle-custom">
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left mb-cus">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                                <div className="f-left">
+                                    <div><img src={toolsjira} className="bitmap-image" alt="Dot" /></div>
+                                    <div className="bimap-title"><small>Jira</small></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
